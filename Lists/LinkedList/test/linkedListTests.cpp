49c8045c7aaa660b1574561d555454fb1ff10405
ptrevordacty1 <66497317+ptrevodactyl@users.noreply.github.com>
2022-08-04 19:25:48 -0700

    more thorough tests

--- Lists/LinkedList/test/linkedListTests.cpp
+++ Lists/LinkedList/test/linkedListTests.cpp
@@ -1,11 +1,34 @@
 #include "../linkedList.hpp"
 #include <gtest/gtest.h>
+#include <crtdbg.h>
 
-TEST(linkedListTests, DemonstrateGTestMacros) {
-    mydata::LinkedList<int> ll;
+#define list mydata::LinkedList
+
+TEST(linkedListTests, append) {
+    list<int> ll;
+    
     ll.append(1);
-    ll.append(2);
-    ll.append(3);
-    ll.append(4);
-    EXPECT_TRUE(ll.size() == 4);
+    EXPECT_TRUE(ll.getSize() == 1);
+    EXPECT_TRUE(ll.getHead() == 1);
+    EXPECT_TRUE(ll.getTail() == 1);
+
+    ll.prepend(3);
+    EXPECT_TRUE(ll.getSize() == 2);
+    EXPECT_TRUE(ll.getHead() == 3);
+    EXPECT_TRUE(ll.getTail() == 1);
+}
+
+TEST(linkedListTests, show) {
+    list<int> ll;
+
+    for(int i = 0; i < 100000; ++i) ll.append(rand() % 100000);
+
+    EXPECT_NO_FATAL_FAILURE(ll.show());
+    EXPECT_NO_FATAL_FAILURE(std::cout << ll);
+}
+
+TEST(linkedListTests, destructor) {
+    list<int> *ll = new list<int>();
+    for(int i = 0; i < 100; ++i) ll->append(rand() % 100);
+    delete ll;
 }
